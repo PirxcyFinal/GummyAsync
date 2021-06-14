b157a704742b1b94ad4acf2d12aabc54a4fc885d
PirxcyFinal <71592833+PirxcyFinal@users.noreply.github.com>
2021-06-14 06:34:02 +0100

    Update __init__.py

--- GummyFNAsync/__init__.py
+++ GummyFNAsync/__init__.py
@@ -68,11 +68,9 @@ class cosmetic_result:
 class cc_result:
     def __init__(self, data: dict) -> None:
         self.data = data
-        self.code = data['code']
         self.account = data['account']
         self.account.name = data['account']['name']
         self.account.id = data['account']['id']
-        self.name = data['code']
         self.status = data['info']['status']
         self.info = data['info']
             
